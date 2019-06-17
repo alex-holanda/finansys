9d6dde122b7bba26cabce0becfb6031e987be76f
Alex Holanda <alex.holanda@outlook.com>
2019-06-17 15:11:34 -0300

    Adicionado BaseResourceListComponent e refatorado EntryListComponent e CategoryListComponent

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,3 +1,4 @@
+import { BaseResourceListComponent } from 'src/app/shared/components/base-resource-list/base-resource-list.component';
 import { Component, OnInit } from '@angular/core';
 
 import { Category } from '../shared/category.model';
@@ -8,26 +9,12 @@ import { CategoryService } from '../shared/category.service';
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent implements OnInit {
+export class CategoryListComponent extends BaseResourceListComponent<Category> {
 
-  categories: Category[] = [];
-
-  constructor(private categoryService: CategoryService) { }
-
-  ngOnInit() {
-    this.categoryService.getAll().subscribe(
-      categories => this.categories = categories,
-      error => alert('Erro ao carregar a lista')
-    )
+  constructor(
+    protected categoryService: CategoryService
+  ) {
+    super(categoryService);
   }
 
-  deleteCategory(category) {
-    const mustDelete = confirm('Deseja realmente excluir este item?');
-    if (mustDelete) {
-      this.categoryService.delete(category.id).subscribe(
-        () => this.categories = this.categories.filter(element => element != category),
-        () => alert('Erro ao tentar excluir')
-      )
-    }
-  }
 }
